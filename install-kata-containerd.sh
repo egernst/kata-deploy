86fa10a993035bc1da040dce06b9efb450d472cd
Eric Ernst <eric.ernst@intel.com>
2018-05-16 14:50:52 -0700

    containerd: update kata configuration script
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- install-kata-containerd.sh
+++ install-kata-containerd.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-## Configure containerd to use crioC
+
+## Configure containerd to use Kata:
+sudo mkdir -p /etc/containerd/
+
+cat << EOT | sudo tee /etc/containerd/config.toml
+[plugins]
+    [plugins.cri.containerd]
+      snapshotter = "overlayfs"
+      [plugins.cri.containerd.default_runtime]
+        runtime_type = "io.containerd.runtime.v1.linux"
+        runtime_engine = ""
+        runtime_root = ""
+      [plugins.cri.containerd.untrusted_workload_runtime]
+        runtime_type = "io.containerd.runtime.v1.linux"
+        runtime_engine = "/opt/kata/bin/kata-runtime"
+        runtime_root = ""
+EOT
+
 
 echo "Reload systemd services"
 sudo systemctl daemon-reload
-
+sudo systemctl restart containerd
