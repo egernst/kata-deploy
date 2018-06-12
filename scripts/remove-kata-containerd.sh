21ea0da2bf52682292b287248ab847c9d3b203d8
Eric Ernst <eric.ernst@intel.com>
2018-06-12 16:12:44 -0700

    Don't reset kubelet, cri-shim in prestophook
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- scripts/remove-kata-containerd.sh
+++ scripts/remove-kata-containerd.sh
@@ -3,7 +3,3 @@ echo "delete kata artifacts"
 rm -rf /opt/kata
 rm -rf /usr/share/defaults/kata-containers
 rm -f /etc/containerd/config.toml
-
-echo "Reload systemd services"
-systemctl daemon-reload
-systemctl restart containerd
--- scripts/remove-kata-crio.sh
+++ scripts/remove-kata-crio.sh
@@ -2,9 +2,4 @@
 echo "deleting kata artifacts"
 rm -rf /opt/kata/
 rm -rf /usr/sahre/defaults/kata-containers
-
 mv /etc/crio/crio.conf.bak /etc/crio/crio.conf
-
-echo "Reload systemd services"
-systemctl daemon-reload
-systemctl restart crio
