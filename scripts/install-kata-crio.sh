4b387bea4fb33377da7824cea253f274d9848a04
Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
2018-06-12 13:23:39 -0700

    Organize scripts, split yaml, bump to 1.0.0 in Dockerfile
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- scripts/install-kata-crio.sh
+++ scripts/install-kata-crio.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-## move Kata artifacts to /opt
 echo "copying kata artifacts from /tmp to /opt"
 cp -R /tmp/kata/* /opt/kata/
 
@@ -8,18 +6,7 @@ chmod +x /opt/kata/bin/*
 
 cp /opt/kata/configuration.toml /usr/share/defaults/kata-containers/configuration.toml
 
-crio_config_file="/etc/crio/crio.conf"
-
-cp $crio_config_file $crio_config_file.bak
-
 cp /etc/crio/crio.conf /etc/crio/crio.conf.bak
-
-## Configure CRIO to use Kata:
-
-### Configure CRIO to use Kata:
-
-## Uncomment next line if you'd like to have default trust level for the cluster be "untrusted":
-# sed -i 's/default_workload_trust = "trusted"/default_workload_trust = "untrusted"/' "$crio_config_file"
 
 echo "Set Kata containers as default runtime in CRI-O for untrusted workloads"
  sed -i 's/runtime_untrusted_workload = ""/runtime_untrusted_workload = "\/opt\/kata\/bin\/kata-runtime"/' /etc/crio/crio.conf
@@ -27,5 +14,3 @@ echo "Reload systemd services"
 
 systemctl daemon-reload
 systemctl restart crio
-
-### Restart CRIO:
--- scripts/remove-kata-crio.sh
+++ scripts/remove-kata-crio.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
-
-## move Kata artifacts to /opt
-echo "copying kata artifacts from /tmp to /opt"
+echo "deleting kata artifacts"
 rm -rf /opt/kata/
 rm -rf /usr/sahre/defaults/kata-containers
-
-crio_config_file="/etc/crio/crio.conf"
-
-cp $crio_config_file $crio_config_file.bak
 
 mv /etc/crio/crio.conf.bak /etc/crio/crio.conf
 
 echo "Reload systemd services"
-
 systemctl daemon-reload
 systemctl restart crio
