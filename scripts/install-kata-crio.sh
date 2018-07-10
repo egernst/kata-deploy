6a872968ad7404428b16e2353165de95dd2d0bee
Eric Ernst <eric.ernst@intel.com>
2018-07-10 19:03:08 +0000

    update install scripts based on new artifact tar
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- scripts/install-kata-crio.sh
+++ scripts/install-kata-crio.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 echo "copying kata artifacts from /tmp to /opt"
-cp -R /tmp/kata/* /opt/kata/
+cp -R /opt/kata-artifacts/bin/* /opt/kata/bin
+
+mv /opt/kata/bin/qemu /opt/kata/share/
 
 chmod +x /opt/kata/bin/*
 
-cp /opt/kata/configuration.toml /usr/share/defaults/kata-containers/configuration.toml
+cp /opt/kata-artifacts/configuration.toml /usr/share/defaults/kata-containers/configuration.toml
+sed -i 's!/usr.*kata-containers/!/opt/kata/bin/!' /usr/share/defaults/kata-containers/configuration.toml
 
 cp /etc/crio/crio.conf /etc/crio/crio.conf.bak
 
 echo "Set Kata containers as default runtime in CRI-O for untrusted workloads"
- sed -i 's/runtime_untrusted_workload = ""/runtime_untrusted_workload = "\/opt\/kata\/bin\/kata-runtime"/' /etc/crio/crio.conf
-echo "Reload systemd services"
+sed -i '/runtime_untrusted_workload = /c\runtime_untrusted_workload = "/opt/kata/bin/kata-runtime"' /etc/crio/crio.conf
 
+echo "Reload systemd services"
 systemctl daemon-reload
 systemctl restart crio
