62295c004855896a817058c5242baf6d4a7b8ca1
Eric Ernst <eric.ernst@intel.com>
2018-07-10 19:33:10 +0000

    pull in updates from review feedback
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- scripts/install-kata-crio.sh
+++ scripts/install-kata-crio.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 echo "copying kata artifacts from /tmp to /opt"
 cp -R /opt/kata-artifacts/bin/* /opt/kata/bin
-
 mv /opt/kata/bin/qemu /opt/kata/share/
-
 chmod +x /opt/kata/bin/*
-
 cp /opt/kata-artifacts/configuration.toml /usr/share/defaults/kata-containers/configuration.toml
-sed -i 's!/usr.*kata-containers/!/opt/kata/bin/!' /usr/share/defaults/kata-containers/configuration.toml
 
-cp /etc/crio/crio.conf /etc/crio/crio.conf.bak
+# Update Kata configuration for /opt/kata path usage
+sed -i 's!/usr.*kata-containers/!/opt/kata/bin/!' /usr/share/defaults/kata-containers/configuration.toml
+sed -i 's!/usr/bin/!/opt/kata/bin/!' /usr/share/defaults/kata-containers/configuration.toml
 
 echo "Set Kata containers as default runtime in CRI-O for untrusted workloads"
+cp /etc/crio/crio.conf /etc/crio/crio.conf.bak
 sed -i '/runtime_untrusted_workload = /c\runtime_untrusted_workload = "/opt/kata/bin/kata-runtime"' /etc/crio/crio.conf
 
 echo "Reload systemd services"
