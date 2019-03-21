3605e3cb5bf952b4d0c63aa6fbf65fb2d9edd556
Eric Ernst <eric.ernst@intel.com>
2019-03-21 16:06:35 -0700

    kata-deploy-action: make entrypoint more modular
    Signed-off-by: Eric Ernst <eric.ernst@intel.com>

--- kata-deploy/action/entrypoint.sh
+++ kata-deploy/action/entrypoint.sh
@@ -4,102 +4,17 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-export AZURE_HTTP_USER_AGENT="GITHUBACTIONS_${GITHUB_ACTION_NAME}_${GITHUB_REPOSITORY}"
-
-YAMLPATH="https://raw.githubusercontent.com/egernst/kata-deploy/$GITHUB_SHA/kata-deploy"
-LOCATION=${LOCATION:-westus2}
-DNS_PREFIX=${DNS_PREFIX:-kata-deploy-${GITHUB_SHA:0:10}}
-CLUSTER_CONFIG=${CLUSTER_CONFIG:-/kubernetes-containerd.json}
-
-function cleanup() {
-#cleanup
-az login --service-principal -u $AZ_APPID -p $AZ_PASSWORD --tenant $AZ_TENANT_ID
-az group delete --name $DNS_PREFIX --yes --no-wait
-az logout
-}
-trap cleanup EXIT
-
-function test-kata() {
-        echo "verify connectivity with a pod using Kata"
-
-        deployment="nginx-deployment"
-        busybox_pod="test-nginx"
-        busybox_image="busybox"
-        cmd="kubectl get pods -a | grep $busybox_pod | grep Completed"
-        wait_time=30
-        sleep_time=3
-
-        # start the kata pod:
-        kubectl apply -f "$YAMLPATH/examples/${deployment}.yaml"
-        kubectl wait --for=condition=Available deployment/${deployment}
-        kubectl expose deployment/${deployment}
-
-        # test pod connectivity:
-        kubectl run $busybox_pod --restart=Never --image="$busybox_image" \
-                -- wget --timeout=5 "$deployment"
-        waitForProcess "$wait_time" "$sleep_time" "$cmd"
-        kubectl logs "$busybox_pod" | grep "index.html"
-        kubectl describe pod "$busybox_pod"
-
-        kubectl delete deployment "$deployment"
-        kubectl delete service "$deployment"
-        kubectl delete pod "$busybox_pod"
-}
-
 die() {
         msg="$*"
         echo "ERROR: $msg" >&2
         exit 1
 }
 
-[[ -z "$AZ_APPID" ]] && die "no Azure service principal ID provided"
-[[ -z "$AZ_PASSWORD" ]] && die "no Azure service principal secret provided"
-[[ -z "$AZ_SUBSCRIPTION_ID" ]] && die "no Azure subscription ID provided"
-[[ -z "$AZ_TENANT_ID" ]] && die "no Azure tenant ID provided"
+source setup-aks.sh
+source test-kata.sh
 
-# check cluster config existence
-# TODO
+trap destroy_aks EXIT
 
-# Give it a try
+setup_aks
 
-aks-engine deploy --subscription-id $AZ_SUBSCRIPTION_ID \
-        --client-id $AZ_APPID --client-secret $AZ_PASSWORD \
-        --location $LOCATION --dns-prefix $DNS_PREFIX \
-        --api-model $CLUSTER_CONFIG --force-overwrite
-
-export KUBECONFIG="_output/kubeconfig/kubeconfig.$LOCATION.json"
-
-#kubectl all the things
-kubectl get pods --all-namespaces
-
-kubectl apply -f "$YAMLPATH/kata-rbac.yaml"
-kubectl apply -f "$YAMLPATH/examples/runtimeclass_crd.yaml"
-kubectl apply -f "$YAMLPATH/examples/kata-runtimeClass.yaml"
-
-sleep 5
-
-kubectl get runtimeclasses
-
-wget  "$YAMLPATH/kata-deploy.yaml"
-wget  "$YAMLPATH/kata-cleanup.yaml"
-
-# update deployment daemonset to utilize the container under test:
-sed -i 's#katadocker/kata-deploy#katadocker/kata-deploy-ci:${GITHUB_SHA}#g' kata-deploy.yaml
-sed -i 's#katadocker/kata-deploy#katadocker/kata-deploy-ci:${GITHUB_SHA}#g' kata-cleanup.yaml
-
-# deploy kata:
-kubectl apply -f kata-deploy.yaml
-
-#wait for kata-deploy to be up
-kubectl  -n kube-system wait --for=condition=Available daemonset/kata-deploy
-
-#Do I see this?
-kubectl get pods --all-namespaces
-
-test-kata
-
-# remove kata (yeah, we are about to destroy, but good to test this flow as well):
-kubectl delete -f kata-deploy.yaml
-kubectl apply -f kata-cleanup.yaml
-kubectl  -n kube-system wait --for=condition=Available daemonset/kata-cleanup
-kubectl delete  -f kata-cleanup.yaml
+test_kata
---
+++ kata-deploy/action/test-kata.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+YAMLPATH="https://raw.githubusercontent.com/egernst/kata-deploy/$GITHUB_SHA/kata-deploy"
+
+function run_test() {
+        echo "verify connectivity with a pod using Kata"
+
+        deployment="nginx-deployment"
+        busybox_pod="test-nginx"
+        busybox_image="busybox"
+        cmd="kubectl get pods -a | grep $busybox_pod | grep Completed"
+        wait_time=30
+        sleep_time=3
+
+        # start the kata pod:
+        kubectl apply -f "$YAMLPATH/examples/${deployment}.yaml"
+        kubectl wait --for=condition=Available deployment/${deployment}
+        kubectl expose deployment/${deployment}
+
+        # test pod connectivity:
+        kubectl run $busybox_pod --restart=Never --image="$busybox_image" \
+                -- wget --timeout=5 "$deployment"
+        waitForProcess "$wait_time" "$sleep_time" "$cmd"
+        kubectl logs "$busybox_pod" | grep "index.html"
+        kubectl describe pod "$busybox_pod"
+
+        kubectl delete deployment "$deployment"
+        kubectl delete service "$deployment"
+        kubectl delete pod "$busybox_pod"
+}
+
+function test_kata(){
+#kubectl all the things
+kubectl get pods --all-namespaces
+
+kubectl apply -f "$YAMLPATH/kata-rbac.yaml"
+kubectl apply -f "$YAMLPATH/examples/runtimeclass_crd.yaml"
+kubectl apply -f "$YAMLPATH/examples/kata-runtimeClass.yaml"
+
+sleep 5
+
+kubectl get runtimeclasses
+
+wget  "$YAMLPATH/kata-deploy.yaml"
+wget  "$YAMLPATH/kata-cleanup.yaml"
+
+# update deployment daemonset to utilize the container under test:
+sed -i 's#katadocker/kata-deploy#katadocker/kata-deploy-ci:${GITHUB_SHA}#g' kata-deploy.yaml
+sed -i 's#katadocker/kata-deploy#katadocker/kata-deploy-ci:${GITHUB_SHA}#g' kata-cleanup.yaml
+
+# deploy kata:
+kubectl apply -f kata-deploy.yaml
+
+#wait for kata-deploy to be up
+kubectl  -n kube-system wait --for=condition=Available daemonset/kata-deploy
+
+#Do I see this?
+kubectl get pods --all-namespaces
+
+run_test
+
+# remove kata (yeah, we are about to destroy, but good to test this flow as well):
+kubectl delete -f kata-deploy.yaml
+kubectl apply -f kata-cleanup.yaml
+kubectl  -n kube-system wait --for=condition=Available daemonset/kata-cleanup
+kubectl delete  -f kata-cleanup.yaml
+}
